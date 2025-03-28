59c7d142c48815a0f84a849d47e0eb693edd21c3
kiranS-2506git config --global user.name kiranS-2506git config --global user.name kiranS-2506 <kiranseru25@gmail.com>
2025-03-28 10:32:35 +0530

    update

--- CRUDwithrest/CRUDwithrest/urls.py
+++ CRUDwithrest/CRUDwithrest/urls.py
@@ -21,5 +21,5 @@ from api.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/', EmployeeCRUD.as_view()),
+    path('api/',EmployeeCRUDCBV.as_view())
 ]
--- CRUDwithrest/api/views.py
+++ CRUDwithrest/api/views.py
@@ -1,27 +1,26 @@
-from django.shortcuts import render
-from api.models import employee
-from django.views.generic import View
-from api.serializers import employeeSerializer
 import io
+from django.views.generic import View
 from rest_framework.parsers import JSONParser
+from api.models import employee
+from api.serializers import employeeSerializer
 from rest_framework.renderers import JSONRenderer
 from django.http import HttpResponse
-# Create your views here.
-class EmployeeCRUD(View):
+class EmployeeCRUDCBV(View):
     def get(self,request,*args,**kwargs):
         json_data = request.body
-        stream =io.BytesIO(json_data)
+        stream = io.BytesIO(json_data)
         pdata = JSONParser().parse(stream)
         id = pdata.get('id',None)
-        if id is not None:
+        if id:
             emp = employee.objects.get(id=id)
             eserializer = employeeSerializer(emp)
             json_data = JSONRenderer().render(eserializer.data)
             return HttpResponse(json_data,content_type='application/json',status=200)
-        qs = employee.objects.all()
-        eserializer = employeeSerializer(qs,many=True)
-        json_data = JSONRenderer().render(eserializer.data)
-        return HttpResponse(json_data,content_type='application/json',status=200)
+        else:
+            qs = employee.objects.all()
+            eserializer = employeeSerializer(qs,many=True)
+            json_data = JSONRenderer().render(eserializer.data)
+            return HttpResponse(json_data,content_type='application/json')
 
 
 
